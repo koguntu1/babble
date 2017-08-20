ccd1cf9b1e0d7a83dc011768a2292f214ba587bb
Keith Oguntuwase <koguntu1@my.westga.edu>
2017-08-19 21:31:56 -0700

    added TestTileGroup class to extend Test Tile Groups for testing

---
+++ src/main/java/edu/westga/cs/babble/model/EmptyTileBagException.java
@@ -0,0 +1,16 @@
+package edu.westga.cs.babble.model;
+
+/**
+ * EmptyTileBagException class
+ * @author Keith Oguntuwase
+ * @version 2.0
+ *
+ */
+public class EmptyTileBagException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7663312457133288050L;
+
+}
